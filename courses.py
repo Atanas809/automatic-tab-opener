0c3feabed3038ac8e0d14835f4c5f4572dd5fce1
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-18 10:35:41 +0200

    Update courses.py

--- courses.py
+++ courses.py
@@ -1,14 +1,5 @@
 
 
-    command = input().split(" : ")
-
-    my_dict = dict()
-
-    while command[0] != "end":
-
-        course = command[0]
-        name = command[1]
-
         if course not in my_dict.keys():
             my_dict[course] = list()
 
