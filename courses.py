65273bb4de6c65dd910de1d51059551bb942e1d8
Atanas Bachev <102140383+Atanas809@users.noreply.github.com>
2022-11-18 10:35:48 +0200

    Update courses.py

--- courses.py
+++ courses.py
@@ -1,12 +1,5 @@
 
 
-        if course not in my_dict.keys():
-            my_dict[course] = list()
-
-        my_dict[course].append(name)
-
-        command = input().split(" : ")
-
     output(my_dict)
 
 softuni_courses()
